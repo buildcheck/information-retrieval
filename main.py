19141a961632f9da24d8b959ff48ecdc8120e8ab
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-09-19 22:52:04 -0700

    Time functions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from functools import partial
 from pathlib import Path
 import pickle
+import time
 import sys
 
 import numpy as np
@@ -7,13 +9,20 @@ from rank_bm25 import BM25Okapi
 from transformers import BertTokenizer
 from tqdm import tqdm
 
-def main(query):
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+def timefunc(msg, func):
+    print(msg, end='')
+    s = time.perf_counter()
+    res = func()
+    print(f'elapsed time: {time.perf_counter() - s:.4f} seconds')
+    return res
 
+def get_tokenized_corpus(tokenizer):
     save_file = Path('tokenized_corpus.pickle')
     if save_file.exists():
-        print('Loading tokenized documents...')
-        tokenized_corpus = pickle.load(save_file.open('rb'))
+        tokenized_corpus = timefunc(
+            'Loading tokenized documents... ',
+            partial(pickle.load, save_file.open('rb'))
+        )
     else:
         tokenized_corpus = [
             tokenizer.tokenize(doc.open().read())
@@ -22,15 +31,23 @@ def main(query):
             ), desc='Tokenizing documents')
         ]
         pickle.dump(tokenized_corpus, save_file.open('wb'))
+    return tokenized_corpus
+
+def main(query):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    tokenized_corpus = get_tokenized_corpus(tokenizer)
+
     tokenized_query = tokenizer.tokenize(query)
     bm25 = BM25Okapi(tokenized_corpus)
-
+    scores = timefunc(
+        'Scoring query against corpus... ',
+        partial(bm25.get_scores, tokenized_query)
+    )
     top_N = 5
-    scores = bm25.get_scores(tokenized_query)
     results = [
-        f'Score: {scores[idx]:.4f}\n'
-        + tokenizer.decode(tokenizer.convert_tokens_to_ids(tokenized_corpus[idx]))
-        for idx in np.argsort(scores)[: -top_N - 1: -1]
+        f'Score: {scores[i]:.4f}\n'
+        + tokenizer.decode(tokenizer.convert_tokens_to_ids(tokenized_corpus[i]))
+        for i in np.argsort(scores)[: -top_N - 1: -1]
     ]
     divider = '\n' + '=' * 40 + '\n'
     print(f'\nTop {top_N} results:')
