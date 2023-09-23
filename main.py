627f8bc2a6e883cab8dcbc966a6db52496154ec9
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-09-22 22:56:37 -0700

    Refactor

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import csv
 from functools import partial
 from pathlib import Path
 import pickle
@@ -39,7 +38,24 @@ def get_tokenized_corpus(tokenizer):
         pickle.dump(tokenized_corpus, save_file.open('wb'))
     return tokenized_corpus
 
+def get_version_hashes():
+    data_hash = timefunc('Hashing data directory... ', partial(
+        subprocess.run,
+        f'tar cf - {DATA_DIR.name} | git hash-object --stdin',
+        stdout=subprocess.PIPE, shell=True, text=True
+    )).stdout.strip()
+    commit_hash = timefunc('Getting code hash... ', partial(
+        subprocess.run,
+        'git rev-parse HEAD',
+        stdout=subprocess.PIPE, shell=True, text=True
+    )).stdout.strip()
+    return data_hash, commit_hash
+
 def main(query):
+    '''
+    If the user provided a query, then get the answers for it and print results
+    Otherwise, read queries from csv and output to csv
+    '''
     tokenizer = AutoTokenizer.from_pretrained('colbert-ir/colbertv2.0')
     tokenized_corpus = get_tokenized_corpus(tokenizer)
     bm25 = timefunc('Initializing BM25... ', partial(BM25Okapi, tokenized_corpus))
@@ -59,49 +75,47 @@ def main(query):
     top_N = 50
     top_idx = np.argsort(scores)[:, : -top_N - 1: -1]
 
+    doc_filenames = sorted(DATA_DIR.glob('*.txt'))
+    # We could generate this by using `tokenizer.decode` on the tokenized
+    # corpus, but it doesn't reconstruct originals perfectly (e.g.
+    # capitalization is all lower case) so we read again directly the originals
+    top_docs = [[
+        f'Source file: {doc_filenames[i].name}\n'
+        + (d := doc_filenames[i].open().read())[d.index('\n') + 1:] for i in r
+    ] for r in top_idx]
+    top_scores = scores[np.arange(scores.shape[0])[:, None], top_idx]
+
     if query:
         results = [
-            f'Score: {scores[0][i]:.4f}\n'
-            + tokenizer.decode(tokenizer.convert_tokens_to_ids(tokenized_corpus[i]))
-            for i in top_idx[0]
+            f'Score: {top_scores[0][i]:.4f}\n' + top_docs[0][i]
+            for i in range(top_scores.shape[1])
         ]
         divider = '\n' + '=' * 40 + '\n'
         print(f'\nBM25 Top {top_N} results:')
         print(divider + divider.join(results))
         print()
     else:
-        untokenized_corpus = sorted(
-            f'Source file: {p.name}\n' + (d := p.open().read())[d.index('\n') + 1:]
-            for p in DATA_DIR.glob('*.txt')
-        )
-        top_docs = [[untokenized_corpus[i] for i in r] for r in top_idx]
-        top_scores = scores[np.arange(scores.shape[0])[:, None], top_idx]
-        df_docs = pd.DataFrame(top_docs, columns=[f"Doc {i}" for i in range(1, top_scores.shape[1] + 1)])
-        df_scores = pd.DataFrame(top_scores, columns=[f"BM25 Score {i}" for i in range(1, top_scores.shape[1] + 1)])
+        df_docs = pd.DataFrame(top_docs, columns=[
+            f"Doc {i}" for i in range(1, top_scores.shape[1] + 1)
+        ])
+        df_scores = pd.DataFrame(top_scores, columns=[
+            f"BM25 Score {i}" for i in range(1, top_scores.shape[1] + 1)
+        ])
 
         # Interleave the two DataFrames and add blank columns
         columns = []
         for i in range(1, df_docs.shape[1] + 1):
             columns.append(df_docs[f"Doc {i}"])
-            columns.append(pd.Series(np.nan, name=f"Expert Score {i}", index=df_docs.index))
+            columns.append(pd.Series(
+                np.nan, name=f"Expert Score {i}", index=df_docs.index
+            ))
             columns.append(df_scores[f"BM25 Score {i}"])
 
         # Concatenate along the columns axis
         df_interleaved = pd.concat(columns, axis=1)
-
-        data_hash = timefunc('Hashing data directory... ', partial(
-            subprocess.run,
-            f'tar cf - {DATA_DIR.name} | git hash-object --stdin',
-            stdout=subprocess.PIPE, shell=True, text=True
-        )).stdout.strip()
-        commit_hash = timefunc('Getting code hash... ', partial(
-            subprocess.run,
-            'git rev-parse HEAD',
-            stdout=subprocess.PIPE, shell=True, text=True
-        )).stdout.strip()
-
         print('Outputting to csv...')
         outfile = Path('ir-answers.csv').open('w')
+        data_hash, commit_hash = get_version_hashes()
         outfile.write(f'Data version: {data_hash},Code version: {commit_hash}\n')
         pd.concat([qcsv, df_interleaved], axis=1).to_csv(outfile, index=False)
         print('Success!')
@@ -110,6 +124,4 @@ if __name__ == '__main__':
     # Avoids potential deadlocks
     os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
-    # If the user provided a query, then get the answers for that prompt
-    # Otherwise, read queries from csv
     main(sys.argv[1] if len(sys.argv) >= 2 else None)
